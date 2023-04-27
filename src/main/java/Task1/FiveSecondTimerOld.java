3c24d6d946982c605eb3530584a394ec1d6a46fa
odin0960 <121524275+odin0960@users.noreply.github.com>
2023-04-27 18:52:24 +0300

    Homework module12 Task1

---
+++ src/main/java/Task1/FiveSecondTimerOld.java
@@ -0,0 +1,44 @@
+package Task1;
+
+import java.util.Scanner;
+
+class FiveSecondTimerOld {
+
+    public static void main(String[] args) {
+
+        long start = System.currentTimeMillis();
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    System.out.println(System.currentTimeMillis() - start);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+
+        new Thread(() -> {
+            try {
+                while (true) {
+//        while(!Thread.currentThread().isInterrupted())
+                    Thread.sleep(5000);
+                    System.out.println("Минуло 5 сек.");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+//        Thread.currentThread().interrupt()
+            }
+        }).start();
+
+
+        Scanner scanner = new Scanner(System.in);  //натиснути Enter, щоб зупинити программу
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals(""))
+                System.exit(0);
+        }
+    }
+}
